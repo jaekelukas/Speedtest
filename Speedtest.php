3e59e396027cffc2c1dcaff70fed54bffb9948f7
jaekelukas <80910021+jaekelukas@users.noreply.github.com>
2021-05-28 11:48:01 +0200

    Add files via upload
    History Tabelle hinzugefügt

--- Speedtest.php
+++ Speedtest.php
@@ -6,62 +6,125 @@
     <title>Speedtest</title>
   </head>
   <body>
+    <?php
+
+      /*
+      $input=fopen("php://input",'r');
+      $json=fgets($input);
+      fclose($input);
+
+        echo $obj;
+        /*echo $obj->ping;
+        echo $obj->download;
+        echo $obj->upload;
+        if ($conn->query($sql) === TRUE) {
+        $last_id = $conn->insert_id;
+        echo "New record created successfully. Last inserted ID is: " . $last_id;
+        } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+
+    }*/
+      $servername = "localhost";
+      $username = "root";
+      $password = "my1stQNAP";
+      $dbname = "Speedtest";
+
+      // Create connection
+      $conn = new mysqli($servername, $username, $password, $dbname);
+
+      // Check connection
+      if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+      }
+      $sql = "INSERT INTO `Ranking` (`Nr`, `IP`, `Download`, `Upload`, `Ping`, `Time`)
+      VALUES ('5', '192.168.1.1', '50 MBits', '50 MBits', '10', '2021-05-27 07:39:31')";
+
+      if ($conn->query($sql) === TRUE) {
+      $last_id = $conn->insert_id;
+      echo "New record created successfully. Last inserted ID is: " . $last_id;
+      } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+     ?>
     <header>
       <h3>Speedtest</h3>
     </header>
-    <section id=datenAnzeige>
-      <button id="startButton" >
-        Start
-      </button>
+    <form class="" action="index.html" method="post">
+      <section id=datenAnzeige>
+        <table>
+          <tr>
+            <th>Download</th>
+            <th>Upload</th>
+            <th>Ping</th>
+          </tr>
+          <tr>
+            <td id="downloadErgebnis"></td>
+            <td id="uploadErgebnis"></td>
+            <td id="pingErgebnis"></td>
+          </tr>
+        </table>
+      </section>
+      <section id="fortschrittsAnzeige">
+        <label for="fortschritt">Fortschritt:</label>
+        <meter id="fortschritt"
+               min="0" max="100" value="0">
+        </meter>
+      </section>
+    </form>
+    <section id="history">
       <table>
-        <tr>
-          <th>Download</th>
-          <th>Upload</th>
-          <th>Ping</th>
-        </tr>
-        <tr>
-          <td id="downloadErgebnis"></td>
-          <td id="uploadErgebnis"></td>
-          <td id="pingErgebnis"></td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Nummer</th>
+            <th>Datum, Zeit</th>
+            <th>IP-Adresse</th>
+            <th>Download</th>
+            <th>Upload</th>
+            <th>Ping</th>
+          </tr>
+        </thead>
+          <tbody>
+          <?php
+            //where Nr=(max(Nr)-$i)
+            if(($history = $conn->query("select * from `Ranking`"))==TRUE)
+            {
+              echo "Test1";
 
+              if (mysqli_num_rows($history) > 0) {
+
+                echo "Test2";
+
+                $counter=0;
+                while(($row = mysqli_fetch_assoc($history))&&($counter++ < 5)) {
+                  echo "<tr>";
+                  echo "<td>".$row["Nr"]."</td>";
+                  echo "<td>".$row["Time"]."</td>";
+                  echo "<td>".$row["IP"]."</td>";
+                  echo "<td>".$row["Download"]."</td>";
+                  echo "<td>".$row["Upload"]."</td>";
+                  echo "<td>".$row["Ping"]."</td>";
+                  echo "</tr>";
+
+                  }
+                }
+              }
+
+            $conn->close();
+           ?>
+
+         </tbody>
       </table>
     </section>
-    <section id=fortschrittsAnzeige>
-      <label for="fortschritt">Fortschritt:</label>
-      <meter id="fortschritt"
-             min="0" max="100" value="0">
-      </meter>
-    </section>
+
     <footer>
+      <article id="buttonAnzeige">
+        <button id="startButton" >
+          Start
+        </button>
+      </article>
     </footer>
   </body>
   <script type="text/javascript" src="Speedtest.js"></script>
-  <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "my1stQNAP";
-    $dbname = "Speedtest";
-    //echo "<p>Connection</p>";
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
-
-    $sql = "INSERT INTO `Ranking` (`Nr`, `IP`, `Download`, `Upload`, `Ping`, `Time`)
-    VALUES ('1', '192.168.1.1', '50 MBits', '50 MBits', '10', '2021-05-27 07:39:31')";
-
-    if ($conn->query($sql) === TRUE) {
-    $last_id = $conn->insert_id;
-    echo "New record created successfully. Last inserted ID is: " . $last_id;
-    } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-
-    $conn->close();
-
-   ?>
  </html>
